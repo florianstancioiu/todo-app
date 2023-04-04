71fb062a120d3dacc6189159dc403dbbac674926
Florian Stancioiu <florinvictorstancioiu@gmail.com>
2023-04-04 12:26:09 +0300

    [WIP] Toggle light/dark themes functionality

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,15 +1,39 @@
-import classes from './Header.module.css';
-import MobileDarkBg from '../images/bg-mobile-dark.jpg';
+import { useEffect, useRef, useState } from 'react';
+import './Header.css';
 import IconSun from '../images/icon-sun.svg';
+import IconMoon from '../images/icon-moon.svg';
+
+const toggleBodyClass = () => {
+  const bodyElement = document.querySelector('body');
+  bodyElement.classList.toggle('light-theme');
+};
 
 const Header = () => {
+  const switchRef = useRef();
+
+  useEffect(() => {
+    switchRef.current.addEventListener('click', toggleBodyClass);
+
+    return () => {
+      switchRef.current.removeEventListener('click', toggleBodyClass);
+    };
+  }, []);
+
   return (
-    <header
-      className={classes.header}
-      style={{ backgroundImage: `url(${MobileDarkBg})` }}
-    >
-      <h1 className={classes.title}>Todo</h1>
-      <img src={IconSun} className={classes['switch-theme-icon']} alt='' />
+    <header className='header'>
+      <h1 className='title'>Todo</h1>
+      <div ref={switchRef} className='switch-wrapper'>
+        <img
+          src={IconSun}
+          className='switch-theme-icon show-light-icon'
+          alt=''
+        />
+        <img
+          src={IconMoon}
+          className='switch-theme-icon show-dark-icon'
+          alt=''
+        />
+      </div>
     </header>
   );
 };
--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,10 +1,18 @@
 import classes from './Todo.module.css';
 import IconCross from '../images/icon-cross.svg';
+import IconCheck from '../images/icon-check.svg';
 
 const Todo = ({ title, complete }) => {
+  const wrapperClasses = complete
+    ? `${classes.wrapper} ${classes.completed}`
+    : classes.wrapper;
+
   return (
-    <div className={classes.wrapper}>
+    <div className={wrapperClasses}>
       <div className={classes.complete}></div>
+      <div className={classes['fully-complete']}>
+        <img className={classes['check-icon']} src={IconCheck} alt='' />
+      </div>
       <p className={classes.title}>{title}</p>
       <img src={IconCross} className={classes.remove} alt='' />
     </div>
